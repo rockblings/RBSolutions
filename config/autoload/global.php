c8596b7313b54666fcdc77d1f08cc3d3c857cf23
Rockblings Software Solutions <infor@rockblings.com>
2014-04-26 03:13:29 -0400

    change database driver 5

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,16 +1,13 @@
 <?php
 return array(
-   $dsn = sprintf(
-	'mysql:dbname=%s;host=%s',
-	get_cfg_var('zend_developer_cloud.db.name'),
-	get_cfg_var('zend_developer_cloud.db.host')
-),
-
-$db = new PDO(
-	$dsn,
-	get_cfg_var('zend_developer_cloud.db.username'),
-	get_cfg_var('zend_developer_cloud.db.password')
-),
+   'db'=> array(
+    'driver' => 'PDO_MYSQL',
+    'dsn'    => 'mysql:dbname=rbsolutions;hostname=rbsolutions-db.my.phpcloud.com',
+    'username'       => 'rbsolutions',
+    'password'       => '888_ChelseaBlues247!',
+    'port'=>'3306' 
+    
+), 
     'service_manager' => array(
         'factories' => array(
             'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
